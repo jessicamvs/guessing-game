d1df71b0abc7206ca25e24306bf96eb8a10e11ab
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2016-04-09 13:02:51 -0700

    Created counter for number of correct answers

--- game.js
+++ game.js
@@ -3,9 +3,11 @@ console.log("User input for userName: " + userName);
 alert("Hello! Welcome to my page " + userName + "!" );
 
 var cityBorn = prompt("Was I born in Vancouver?");
+var counter = 0;
 console.log("User input for cityBorn: " + cityBorn);
 if (cityBorn.toUpperCase() === "NO") {
   alert("That's right, " + userName + "! I was born in Seattle!");
+  counter += 1;
 } else {
   alert("I wasn't born in Vancouver, " + userName + ". I was born in Seattle!");
 }
@@ -14,6 +16,7 @@ var catOwner = prompt("Do I have a cat?");
 console.log("User input for catOwner: " + catOwner);
 if (catOwner.toUpperCase() === "YES") {
   alert("That's right, " + userName + "!");
+  counter += 1;
 } else {
   alert("Ooops! I do have a cat, " + userName + ".");
 }
@@ -22,9 +25,12 @@ var hairColor = prompt("Is my hair dark?");
 console.log("User input for hairColor: " + hairColor);
 if (hairColor.toUpperCase() === "YES") {
   alert("Yay, " + userName + "! You got it right!");
+  counter += 1;
 } else {
   alert("Sorry, wrong answer. I am not blonde, " + userName + "!");
 }
+
+alert("You got " + counter + " out of 3 answers correct!");
 
 var age = prompt("How old am I?");
 console.log("User input for age: " + age);
