c80eedcbb91bb5687647ae229e9c28d925437578
geoffreycc <gcox3d@gmail.com>
2016-04-13 21:26:03 -0700

    Added fourth question.

--- game.js
+++ game.js
@@ -2,54 +2,69 @@ var userName = prompt("What is your name?");
 console.log("User input for userName: " + userName);
 alert("Hello! Welcome to my page " + userName + "!" );
 
-
 var counter = 0;
+
 var questions = [
   "Was I born in Vancouver?",
   "Do I have a cat?",
   "Is my hair dark?",
   "Guess my number.",
 ];
+
 var correctAn = [
   "That's right, " + userName + "! I was born in Seattle!",
   "That's right, " + userName + "!",
   "Yay, " + userName + "! You got it right!",
 ];
-var wrongAn = [
+
+var wrongAns = [
   "I wasn't born in Vancouver, " + userName + ". I was born in Seattle!",
   "Ooops! I do have a cat, " + userName + ".",
   "Sorry, wrong answer. I am not blonde, " + userName + "!",
 ];
+
 var answers = [
   "NO",
   "YES",
   "YES",
 ];
+
+var wrongAnswers = [
+  "YES",
+  "NO",
+  "NO",
+];
+
 var el = [
   document.getElementById('one'),
   document.getElementById('two'),
   document.getElementById('three'),
 ];
 
-function game(questions, answers, wrongAn, correctAn){
+function game(questions, answers, wrongAns, correctAn){
   var userInput = (prompt(questions).toUpperCase());
-  console.log(userInput);
-  if (userInput === answers) {
-    counter++;
-    el[i].textContent = correctAn;
+  if (isNaN(parseInt(userInput))) {
+    console.log(userInput);
+    if (userInput === answers) {
+      counter++;
+      el[i].textContent = correctAn;
+    } else {
+      el[i].textContent = wrongAns;
+    }
   } else {
-    el[i].textContent = wrongAn;
+    // var num = parseInt(prompt("Guess my number."));
+    while (userInput !== 7) {
+      userInput = parseInt(prompt("Guess my number again."));
+    }
   }
 }
-
-
 // else if (parseInt(userInput) !== 'string') {
 //   while (userInput !== 7) {
 //     userInput = parseInt(prompt("Guess my number again."));
 //   }
 
 for (i = 0; i < questions.length; i++){
-  game(questions[i],answers[i], wrongAn[i], correctAn[i], el[i]);
+  game(questions[i],answers[i], wrongAns[i], correctAn[i], el[i]);
 }
 score.textContent = ("You got " + counter + " out of 4 answers correct!");
 //
