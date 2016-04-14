2f4488ec0b553a271367cd4351cb45a7c887c10a
geoffreycc <gcox3d@gmail.com>
2016-04-13 20:11:44 -0700

    Added to function the ability to write to document.

--- game.js
+++ game.js
@@ -2,11 +2,13 @@ var userName = prompt("What is your name?");
 console.log("User input for userName: " + userName);
 alert("Hello! Welcome to my page " + userName + "!" );
 
+
 var counter = 0;
 var questions = [
   "Was I born in Vancouver?",
   "Do I have a cat?",
   "Is my hair dark?",
+  "Guess my number.",
 ];
 var correctAn = [
   "That's right, " + userName + "! I was born in Seattle!",
@@ -22,61 +24,36 @@ var answers = [
   "NO",
   "YES",
   "YES",
+  7,
+];
+var el = [
+  document.getElementById('one'),
+  document.getElementById('two'),
+  document.getElementById('three'),
 ];
 
 function game(questions, answers, wrongAn, correctAn){
   var userInput = prompt(questions).toUpperCase();
+  console.log(typeof userInput);
   if (userInput === answers) {
-    alert(correctAn);
+    counter++;
+    console.log(typeof userInput);
+    el[i].textContent = correctAn;
+  } else if (parseInt(userInput) !== 'string') {
+    console.log(userInput);
   } else {
-    alert(wrongAn);
+    while (num !== 7) {
+      num = parseInt(prompt("Guess my number again."))
+    }
+    el[i].textContent = wrongAn;
   }
 }
 
-for (i = 0; i < questions.length; i++){
-  game(questions[i],answers[i], wrongAn[i], correctAn[i]);
-}
 
-// var cityBorn = prompt("Was I born in Vancouver?");
-// var counter = 0;
-// console.log("User input for cityBorn: " + cityBorn);
-// if (cityBorn.toUpperCase() === "NO" || cityBorn.toUpperCase() === "N") {
-//   alert("That's right, " + userName + "! I was born in Seattle!");
-//   counter++;
-// } else {
-//   alert("I wasn't born in Vancouver, " + userName + ". I was born in Seattle!");
-// }
-//
-// var catOwner = prompt("Do I have a cat?");
-// console.log("User input for catOwner: " + catOwner);
-// if (catOwner.toUpperCase() === "YES" || catOwner.toUpperCase() === "Y") {
-//   alert("That's right, " + userName + "!");
-//   counter++;
-// } else {
-//   alert("Ooops! I do have a cat, " + userName + ".");
-// }
-//
-// var hairColor = prompt("Is my hair dark?");
-// console.log("User input for hairColor: " + hairColor);
-// if (hairColor.toUpperCase() === "YES" || catOwner.toUpperCase() === "Y") {
-//   alert("Yay, " + userName + "! You got it right!");
-//   counter++;
-// } else {
-//   alert("Sorry, wrong answer. I am not blonde, " + userName + "!");
-// }
-//
-// var age = parseInt(prompt("How old am I?"));
-// console.log("User input for age: " + age);
-// if (age === 26) {
-//   alert("You guessed, right, " + userName + "!");
-//   counter++;
-// } else if (age < 26) {
-//   alert("That guess is too low.");
-// } else {
-//   alert("That guess is too high.");
-// }
-//
-// alert("You got " + counter + " out of 4 answers correct!");
+for (i = 0; i < questions.length; i++){
+  game(questions[i],answers[i], wrongAn[i], correctAn[i], el[i]);
+}
+score.textContent = ("You got " + counter + " out of 4 answers correct!");
 //
 // var num = parseInt(prompt("Guess my number."));
 // while (num !== 7) {
